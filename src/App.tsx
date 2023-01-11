3e7db9ee7e31c3dd98c4563353d3e6226e74f66c
Klaudia Sagan <klaudiasagan.code@gmail.com>
2023-01-11 12:34:52 +0100

    added static data

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <table>
+               <thead>
+                  <tr>
+                     <th>Surname</th> 
+                     <th>Name</th> 
+                     <th>Gender</th> 
+                     <th>Nationality</th> 
+                     <th>Profession</th> 
+                     <th>Marital status</th>
+                  </tr>
+               </thead>
+               <tbody>
+                <tr>
+                  <td>Nowak</td>
+                  <td>Karolina</td>
+                  <td>Female</td>
+                  <td>PL</td>
+                  <td>Actor</td>
+                  <td>Married</td>
+                </tr>
+                <tr>
+                  <td>Gonzales</td>
+                  <td>Tom</td>
+                  <td>Male</td>
+                  <td>DE</td>
+                  <td>Footballer</td>
+                  <td>Divorced</td>
+                </tr>
+                <tr>
+                  <td>Walters</td>
+                  <td>Rose</td>
+                  <td>Female</td>
+                  <td>FR</td>
+                  <td>Cook</td>
+                  <td>Widow</td>
+                </tr>
+                <tr>
+                  <td>King</td>
+                  <td>Adam</td>
+                  <td>Male</td>
+                  <td>UE</td>
+                  <td>Soldier</td>
+                  <td>Married</td>
+                </tr>
+               </tbody>
+            </table>
     </div>
   );
 }
